0ecf86201dd8909cb7ff43666010be428e7b77f4
stathampen <33280323+stathampen@users.noreply.github.com>
2018-06-28 13:55:11 +0100

    updated movement script

--- FunProject/Assets/scripts/CharacterMovement.cs
+++ FunProject/Assets/scripts/CharacterMovement.cs
@@ -6,13 +6,20 @@ public class CharacterMovement : MonoBehaviour {
 
     public float movementSpeed = 10;
     public float turningSpeed = 60;
+    public float jumpHeight = 10;
+    public Rigidbody rigidbody;
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
-        transform.Rotate(0, horizontal, 0);
+        float horizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        transform.Translate(horizontal, 0, 0);
 
         float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
         transform.Translate(0, 0, vertical);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
+        }
     }
 }
