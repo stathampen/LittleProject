fede83e80660f421bc7c74f12f99ef562b666ffe
stathampen <33280323+stathampen@users.noreply.github.com>
2018-07-02 09:57:32 +0100

    Camera movement via mouse
    can make the camera move around the character based on mouse position however it doesn't change where the character is pointing
    doesn't work along the y axis from settings in the camera look script

---
+++ FunProject/Assets/scripts/CameraMouseMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouseMovement : MonoBehaviour {
+
+    public GameObject mainCamera;
+    public float cameraSpeed = 2.0f;
+
+    public float yaw = 0.0f;
+    public float pitch = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// moves the camera based on where the mouse is pointed
+	void Update () {
+        yaw += cameraSpeed * Input.GetAxis("Mouse X");
+        pitch -= cameraSpeed * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+	}
+}
